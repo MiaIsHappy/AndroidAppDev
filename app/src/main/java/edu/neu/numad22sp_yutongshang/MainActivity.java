16cd05aac129424ad930c276a83b0ef5df224e03
Yutong Shang <shang.yut@northeastern.edu>
2022-01-21 20:25:42 -0800

    Add the button and click button action

--- app/src/main/java/edu/neu/numad22sp_yutongshang/MainActivity.java
+++ app/src/main/java/edu/neu/numad22sp_yutongshang/MainActivity.java
@@ -3,6 +3,9 @@ package edu.neu.numad22sp_yutongshang;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void clickButton(android.view.View view){
+        Toast.makeText(getApplicationContext(),"Yutong Shang", Toast.LENGTH_SHORT).show();
     }
 }
