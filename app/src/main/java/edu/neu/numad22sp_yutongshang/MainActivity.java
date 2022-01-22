920f71548a1fcc8e64f521fab351e9ea55dbbbb6
Yutong Shang <shang.yut@northeastern.edu>
2022-01-21 17:51:55 -0800

    Initial MyApplication project

---
+++ app/src/main/java/edu/neu/numad22sp_yutongshang/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.numad22sp_yutongshang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
