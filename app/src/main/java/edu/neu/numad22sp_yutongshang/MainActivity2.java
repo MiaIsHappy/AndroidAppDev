c04fb2620298314e5e861cc3a6c5a1c5805e6c8b
Yutong Shang <shang.yut@northeastern.edu>
2022-02-05 10:47:00 -0800

    Week3 part2 create activity

--- app/src/main/java/edu/neu/numad22sp_yutongshang/MainActivity2.java
+++ app/src/main/java/edu/neu/numad22sp_yutongshang/MainActivity2.java
@@ -1,51 +1,71 @@
 package edu.neu.numad22sp_yutongshang;
 
-import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+import android.os.Bundle;
 import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import edu.neu.numad22sp_yutongshang.databinding.ActivityMain2Binding;
+import android.widget.Button;
+import android.widget.Toast;
+import android.content.Intent;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMain2Binding binding;
-
     @Override
+   // protected void onCreate(Bundle savedInstanceState) {
+     //   super.onCreate(savedInstanceState);
+     //   setContentView(R.layout.activity_main2);
+    //}
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main2);
 
-        binding = ActivityMain2Binding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText("PRESSED:-");
 
-        setSupportActionBar(binding.toolbar);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        Button btn_A = (Button) findViewById(R.id.A);
+        btn_A.setOnClickListener( new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            textView.setText("PRESSED:A");
+        }});
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+        Button btn_B = (Button) findViewById(R.id.B);
+        btn_B.setOnClickListener( new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+            public void onClick(View v) {
+                textView.setText("PRESSED:B");
+            }});
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+        Button btn_C = (Button) findViewById(R.id.C);
+        btn_C.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("PRESSED:C");
+            }});
+
+        Button btn_D = (Button) findViewById(R.id.D);
+        btn_D.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("PRESSED:D");
+            }});
+
+        Button btn_E = (Button) findViewById(R.id.E);
+        btn_E.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("PRESSED:E");
+            }});
+
+        Button btn_F = (Button) findViewById(R.id.F);
+        btn_F.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("PRESSED:F");
+            }});
     }
 }
