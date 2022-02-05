7f19aec8c21cb2fe633ea833e64c1c45933f3d90
Yutong Shang <shang.yut@northeastern.edu>
2022-02-04 22:32:11 -0800

    Week3 part1 create activity

--- app/src/main/java/edu/neu/numad22sp_yutongshang/MainActivity.java
+++ app/src/main/java/edu/neu/numad22sp_yutongshang/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +15,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void clickButton(android.view.View view){
-        Toast.makeText(getApplicationContext(),"Yutong Shang shang.yut@northeastern.edu", Toast.LENGTH_SHORT).show();
-    }
+   // public void clickButton(android.view.View view){
+    //    Toast.makeText(getApplicationContext(),"Yutong Shang shang.yut@northeastern.edu", Toast.LENGTH_SHORT).show();
+    //}
+   public void clickButton(android.view.View view){
+       Button bt_1 = (Button) findViewById(R.id.btnAboutMe);
+
+       bt_1.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v){
+               Toast.makeText(getApplicationContext(),"Yutong Shang shang.yut@northeastern.edu", Toast.LENGTH_SHORT).show();
+           }
+       });
+       Button bt_2 = (Button) findViewById(R.id.button);
+
+       bt_2.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v){
+               startActivity(new Intent(MainActivity.this, MainActivity2.class));
+           }
+       });}
 }
