b065ecef9093ab35d3eb07bdb19c5918689c282c
Yutong Shang <shang.yut@northeastern.edu>
2022-02-05 13:42:48 -0800

    Week3 fix a onClick error

--- app/src/main/java/edu/neu/numad22sp_yutongshang/MainActivity.java
+++ app/src/main/java/edu/neu/numad22sp_yutongshang/MainActivity.java
@@ -14,25 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        Button bt_1 = (Button) findViewById(R.id.btnAboutMe);
+        Button bt_2 = (Button) findViewById(R.id.button);
+
+        bt_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Toast.makeText(getApplicationContext(),"Yutong Shang shang.yut@northeastern.edu", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bt_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, MainActivity2.class));
+            }
+        });}
+
+
    // public void clickButton(android.view.View view){
     //    Toast.makeText(getApplicationContext(),"Yutong Shang shang.yut@northeastern.edu", Toast.LENGTH_SHORT).show();
     //}
-   public void clickButton(android.view.View view){
-       Button bt_1 = (Button) findViewById(R.id.btnAboutMe);
 
-       bt_1.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v){
-               Toast.makeText(getApplicationContext(),"Yutong Shang shang.yut@northeastern.edu", Toast.LENGTH_SHORT).show();
-           }
-       });
-       Button bt_2 = (Button) findViewById(R.id.button);
 
-       bt_2.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v){
-               startActivity(new Intent(MainActivity.this, MainActivity2.class));
-           }
-       });}
+
 }
