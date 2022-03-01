80f78e5108e7811da25191578563e129e8282a81
Yutong Shang <shang.yut@northeastern.edu>
2022-03-01 13:03:34 -0800

    Commit from GH Desktop

---
+++ app/build/generated/source/buildConfig/debug/edu/neu/numad22sp_yutongshang/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package edu.neu.numad22sp_yutongshang;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "edu.neu.numad22sp_yutongshang";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
